b478c71dcb9e055ff6811fb81eb99ac38911bb1e
Andre Wonasue <awonasue@terpmail.umd.edu>
2025-12-12 12:47:08 -0500

    one man army, layout

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Comics-Hero
 This is a repo for INST377 final project. A  website dealing with comics and superheros
+# Title: Comic Heros
+
+# Description of your project
+## - This project focuses on comic books and super heros that most of hus have grown up loving and want to learn more about.
+
+# Description of target browsers (iOS? Android? Which ones?)
+- This webpage is meant to run mainly on desktops or ios devices
+
+# Link to Developer Manual
---
+++ project_hero.js
@@ -0,0 +1,24 @@
+// // Dark Mode not working yet
+
+// const options = {
+//   bottom: '64px', // default: '32px'
+//   right: 'unset', // default: '32px'
+//   left: '32px', // default: 'unset'
+//   time: '0.5s', // default: '0.3s'
+//   mixColor: '#fff', // default: '#fff'
+//   backgroundColor: '#fff',  // default: '#fff'
+//   buttonColorDark: '#100f2c',  // default: '#100f2c'
+//   buttonColorLight: '#fff', // default: '#fff'
+//   saveInCookies: false, // default: true,
+//   label: '🌓', // default: ''
+//   autoMatchOsTheme: true // default: true
+// }
+
+// const darkmode = new Darkmode(options);
+
+// document.getElementById(darkMode)
+// darkmode.showWidget();
+// console.log(darkmode.isActivated()) // will return true 
+
+// // Dark Mode END
+
