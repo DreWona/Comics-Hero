8424eb1d2949312b44399102e51a692baabbea63
daphotoman <wonphoto18@gmail.com>
2025-12-19 01:33:31 -0500

    Commit before transfering all files to public, express/nodejs folders.

--- README.md
+++ README.md
@@ -3,9 +3,29 @@ This is a repo for INST377 final project. A  website dealing with comics and sup
 # Title: Comic Heros
 
 # Description of your project
-## - This project focuses on comic books and super heros that most of hus have grown up loving and want to learn more about.
+- This project focuses on comic books and super heros that most of us have grown up loving and want to learn more about.
+-
+-
+-
+-
 
 # Description of target browsers (iOS? Android? Which ones?)
 - This webpage is meant to run mainly on desktops or ios devices
 
 # Link to Developer Manual
+## How to install your application and all dependencies
+- 
+## How to run your application on a server
+-  
+## How to run any tests you have written for your software
+- 
+## The API for your server application - all GET, POST, PATCH, etc endpoints, and what they each do
+- 
+## A clear set of expectations around known bugs and a road-map for future development.
+- 
+
+# Links to resources
+- https://darkmodejs.learn.uno/
+- https://emojipedia.org/bat
+- https://icons8.com/icon/61992/batman
+
--- project_hero.js
+++ project_hero.js
@@ -1,24 +1,40 @@
-// // Dark Mode not working yet
+// DARK Mode STAR
+function darkmode() {
+    const options = {
+        bottom: '64px', // default: '32px'
+        right: '32px', // default: '32px'
+        left: 'unset', // default: 'unset'
+        time: '0.5s', // default: '0.3s'
+        mixColor: '#fff', // default: '#fff'
+        backgroundColor: '#fff',  // default: '#fff'
+        buttonColorDark: '#100f2c',  // default: '#100f2c'
+        buttonColorLight: '#fff', // default: '#fff'
+        saveInCookies: true, // default: true,
+        label: '<img width="48" height="48" src="https://img.icons8.com/color/48/batman.png" alt="batman"/>', // default: '🌓'
+        autoMatchOsTheme: true // default: true
+    };
 
-// const options = {
-//   bottom: '64px', // default: '32px'
-//   right: 'unset', // default: '32px'
-//   left: '32px', // default: 'unset'
-//   time: '0.5s', // default: '0.3s'
-//   mixColor: '#fff', // default: '#fff'
-//   backgroundColor: '#fff',  // default: '#fff'
-//   buttonColorDark: '#100f2c',  // default: '#100f2c'
-//   buttonColorLight: '#fff', // default: '#fff'
-//   saveInCookies: false, // default: true,
-//   label: '🌓', // default: ''
-//   autoMatchOsTheme: true // default: true
-// }
-
-// const darkmode = new Darkmode(options);
-
-// document.getElementById(darkMode)
-// darkmode.showWidget();
-// console.log(darkmode.isActivated()) // will return true 
-
+    const darkmode = new Darkmode(options);
+    darkmode.showWidget();
+}
+window.addEventListener('load', darkmode);
 // // Dark Mode END
 
+
+
+
+
+
+
+
+
+
+// Search hero and API
+// const hero_APIkey 
+
+// classs s
+
+// async function hero_Search(params) {
+    
+// }
+// fetch()
